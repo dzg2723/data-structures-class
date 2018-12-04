b81638fef23f6cf19807c9c514f47c198bff5b78
Darren <darren.good@emu.edu>
2018-12-04 01:11:44 -0500

    Added drawing documentation, fixed fill method to handle multidigit values

--- ExpTree.java
+++ ExpTree.java
@@ -1,5 +1,5 @@
-import java.util.Stack;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.LinkedList;
 public class ExpTree {
 
@@ -21,36 +21,14 @@ public class ExpTree {
 			ExprNode(char c, ExprNode l, ExprNode r)  { val = 0; op = c; left = l; right = r; }
 		}
 
-		class TokenString {
-			String data;
-			
-			public TokenString(String text) {
-				data = text;
-			}
-			
-			public char nextToken() {
-				if (data.length() == 0) return ' ';
-				char x = data.charAt(0);
-				data = data.substring(1);
-				if (x == ' ') return nextToken();
-				return x;
-			}
-		}
-
-		
-		class IndexedString {
-			public String data;
-			public int i;
-
-			IndexedString(String text) {
-				data = text;
-				i = 0;
-			}
-		}
-
 		class Position {
 			public int val;
+			public int nextval() {
+				return val + 1;
+			}
 		}
+		
+		
 
 		ExprNode root;
 
@@ -65,50 +43,45 @@ public class ExpTree {
 
 		public void fill(String prefixExpr) {
 			root = fill(prefixExpr, new Position());
-			//root = fill(new IndexedString(prefixExpr));
-			//root = fill(new TokenString(prefixExpr));
 		}
-
-		ExprNode fill(String prefixExpr, Position i)
-		{
+		
+		private ExprNode fill(String prefixExpr, Position i) {
+			
+			//loop through until c is an operator or number
 			char c = ' ';
 			while (i.val < prefixExpr.length() && c != '+' && c != '*' && !(c >= '0' && c <= '9')) {
 				c = prefixExpr.charAt(i.val);
 				i.val++;
 			}
+			
+			//end method once expression has been iterated through
 			if (i.val > prefixExpr.length()) return null;
 
+			//Create operator node connected to next two nodes
 			if (c == '+' || c == '*') {
 				return new ExprNode(c, fill(prefixExpr, i), fill(prefixExpr, i));
+				
+			//c is a number
 			} else {
-				return new ExprNode(c - '0');
-			}
-		}
-
-		ExprNode fill(IndexedString prefixExpr)
-		{
-			char c = ' ';
-			while (prefixExpr.i < prefixExpr.data.length() && c != '+' && c != '*' && !(c >= '0' && c <= '9')) {
-				c = prefixExpr.data.charAt(prefixExpr.i);
-				prefixExpr.i++;
-			}
-			if (prefixExpr.i > prefixExpr.data.length()) return null;
-
-			if (c == '+' || c == '*') {
-				return new ExprNode(c, fill(prefixExpr), fill(prefixExpr));
-			} else {
-				return new ExprNode(c - '0');
-			}
-		}
-
-		ExprNode fill(TokenString prefixExpr) {
-			char c = prefixExpr.nextToken();
-			if (c == ' ') { 
-				return null;
-			} else if (c == '+' || c == '*') {
-				return new ExprNode(c, fill(prefixExpr), fill(prefixExpr));
-			} else {
-				return new ExprNode(c - '0');
+				String value = "";	//Place characters into a string for multi-digit numbers
+				value += c;
+				
+				//Look for more digits
+				while (i.val < prefixExpr.length()) {
+					c = prefixExpr.charAt(i.val);
+					
+					//if (next_c == '.'){
+						//do something
+					//}
+					
+					if (c >= '0' && c <= '9') {
+						value += c;
+						i.val++;
+					} else {
+						break;
+					}
+				}
+				return new ExprNode(Integer.parseInt(value));
 			}
 		}
 
@@ -134,7 +107,7 @@ public class ExpTree {
 			String result = "";
 			String e = q.remove();
 			while (true) {
-				result = result + e + " ";
+				result = result + e;
 				try {
 					e = q.remove();
 				} catch (java.util.NoSuchElementException er) {
@@ -244,10 +217,10 @@ public class ExpTree {
 		}
 		
 		public void drawTree() {
+			//https://stackoverflow.com/questions/1235179/simple-way-to-repeat-a-string-in-java
 			eval(); //Make sure that the tree is filled out.
 			int tree_height = getTreeHeight(root);
-			//System.out.print("Tree Height: ");
-			//System.out.println(tree_height);
+			System.out.println("Height: " + tree_height);
 			
 			//Handle exceptions
 			if (tree_height == 0) {
@@ -266,100 +239,89 @@ public class ExpTree {
 			Queue<Integer> col_queue = new LinkedList<>();
 			int starting_column = (int) (5 * Math.pow(2, tree_height-2));
 			col_queue.add(starting_column);
+		
 
-
-			//https://stackoverflow.com/questions/1235179/simple-way-to-repeat-a-string-in-java
-			//String repeated = new String(new char[100]).replace("\0", "abcde");
-			//System.out.println(repeated);
-			
-
-
+			//Print each level of the tree (each level prints three lines except final level)
 			for (int i = 0; i < tree_height; i++) {
 				
 				//Line 1/3 of the set
 				String line = "";			//Progressively assembled before being printed
 				int operator_counter = 0;	//Records the number of nodes on line that are operators
-				int curr_col = 0;
-				int target_col;
-				ExprNode curr_node;
+				int target_col;				//Significant slots in the line string
+				ExprNode curr_node;			//Current node being formatted in the line
 				
-				int potent_nodes = (int) (Math.pow(2, i));
+				int potent_nodes = (int) (Math.pow(2, i)); 	//Possible amount of nodes that may exist at current depth
 				for (int j = 0; j < potent_nodes; j++) {
+					
+					//Get the next node, skip iteration if null
 					curr_node = node_queue.remove();
 					if (curr_node == null) {
 						continue;
 					}
 					target_col = col_queue.remove();
 
-					
-					int num_of_spaces = (target_col-1) - curr_col;
+					//Prepare correct number of spaces for positioning
+					int num_of_spaces = (target_col-1) - line.length();
 					String spaces = new String(new char[num_of_spaces]).replace("\0", " ");
 				
+					//For nodes with no operator, just display their value
 					if (curr_node.op == ' ') {
 						line += spaces + curr_node.val;
-						curr_col += num_of_spaces + 1; //+1 to account for value slot
 						
+					//For operator nodes, display their operator followed by their value
 					} else {
 						line += spaces + curr_node.op + " (" + curr_node.val + ")";
-						int val_digits = Integer.toString(curr_node.val).length();
-						curr_col += num_of_spaces + 3 + val_digits + 1;
 						
-						int step =  (int) (starting_column / Math.pow(2, i+1)); //todo, make it ceil?
-						col_queue.add(target_col - step);
-						col_queue.add(target_col);
-						col_queue.add(target_col + step);
+						//Prepare for next line
+						int step =  (int) (starting_column / Math.pow(2, i+1));	//Horizontal distance branches to children must go
+						col_queue.add(target_col - step);						//column where left child will be
+						col_queue.add(target_col);								//column where current operator is
+						col_queue.add(target_col + step);						//column where right child will be
 						
-						operator_counter += 1;
+						operator_counter += 1;									//Used to iterate correct number of times for line 2/3
 						
 					}
+					//Add child nodes to queue for next loop
 					node_queue.add(curr_node.left);
 					node_queue.add(curr_node.right);
 				}
-				
 				System.out.println(line);
 				
 				//Line 2/3 of the set
 				line = "";
-				curr_col = 0;
 				for (int j = 0; j < operator_counter; j++) {
-					target_col = col_queue.remove();
-					int num_of_cols = target_col - curr_col; 			//we include target column as a space
-					String columns = new String(new char[num_of_cols]).replace("\0", " ");
-					line += columns;
-					curr_col += num_of_cols;
-					col_queue.add(target_col);
 					
-					target_col = col_queue.remove();
-					num_of_cols = (target_col-1) - curr_col;
-					columns = new String(new char[num_of_cols]).replace("\0", "_");
-					line += columns + "|";
-					curr_col += num_of_cols + 1;
+					//Fill spaces leading up to next parent/child connection
+					target_col = col_queue.remove();										//Grab next target column
+					int num_of_cols = target_col - line.length(); 							//include target column as a space
+					String columns = new String(new char[num_of_cols]).replace("\0", " ");	//Make spaces up to and including target column
+					line += columns;														//Add the spaces to the string
+					col_queue.add(target_col);												//Put target column back in queue for next line
 					
+					//Draw left side of connection between parent/child node
+					target_col = col_queue.remove();										//Grab next target column
+					num_of_cols = (target_col-1) - line.length();							//Count columns up to target
+					columns = new String(new char[num_of_cols]).replace("\0", "_");			//Prepare correct number of underscores
+					line += columns + "|";													//Add underscores and the line up to parent
+					
+					//Draw right side of connection between parent/child node
 					target_col = col_queue.remove();
-					num_of_cols = (target_col-1) - curr_col;
-					//System.out.print("Number of columns: ");
-					//System.out.println(num_of_cols);
+					num_of_cols = (target_col-1) - line.length();
 					columns = new String(new char[num_of_cols]).replace("\0", "_");
 					line += columns;
-					curr_col += num_of_cols;
 					col_queue.add(target_col);
 				}
 				System.out.println(line);
 				
 				//Line 3/3 of the set
+				//Place vertical lines above where the nodes will be in next line
 				line = "";
-				curr_col = 0;
 				int num_of_cols;
 				for (int j = 0; j < operator_counter*2; j++) {
 					target_col = col_queue.remove();
-					num_of_cols = (target_col-1) - curr_col;
-					//System.out.print("Target: ");
-					//System.out.println(target_col);
-					//System.out.print("Number of columns: ");
-					//System.out.println(num_of_cols);
+					num_of_cols = (target_col-1) - line.length();
 					String columns = new String(new char[num_of_cols]).replace("\0", " ");
 					line += columns + "|";
-					curr_col += num_of_cols + 1;
 					col_queue.add(target_col);
 				}
 				System.out.println(line);
@@ -368,20 +330,25 @@ public class ExpTree {
 	
 	
 	public static void main(String[] args) {
+		
+		Scanner console_input = new Scanner(System.in);
+		System.out.println("Please enter a prefix notation with integers");
+		String user_expression = console_input.nextLine();
+		
 		ExpTree tree = new ExpTree();
-		tree.fill("+ * + * 8 7 4 5 * + 2 2 * 3 7");
+		//tree.fill("+ * + * 8 7 4 5 * + 2 2 * 3 7");
+		tree.fill(user_expression);
 		
 		System.out.println(tree);
-		//System.out.println(tree.eval());
-		
-		//String prefix = tree.toPrefix();
-		//String postfix = tree.toPostfix();
-		//String infix = tree.toInfix();
-		
-		//System.out.println(prefix);
-		//System.out.println(postfix);
-		//System.out.println(infix);
 		tree.drawTree();
+		
+		String prefix = tree.toPrefix();
+		String postfix = tree.toPostfix();
+		String infix = tree.toInfix();
+		
+		System.out.println(prefix);
+		System.out.println(postfix);
+		System.out.println(infix);
 	}
 
 }
